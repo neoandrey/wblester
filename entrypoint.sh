628e12733e93159d8028908c17d2ea5adb89a014
Bolaji Aina <neoandrey@yahoo.com>
2025-03-12 16:57:19 -0300

    Change session type from redis to filesystem 04

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,18 @@
-#!/bin/bash
+#/bin/bash
 MODE=$1
 echo "MODE: $MODE"
-
-if [ "$MODE" == "both" ]
-then
-    #python -m flask run -p $PORT 
-    python redis_worker.py & gunicorn -w 1 -b 0.0.0.0:$PORT --timeout 120 --threads=3 --worker-connections=1000 $FLASK_APP:app
-   
-else
-
-    if [ "$MODE" == "web" ]
-    then
-        #python -m flask run -p $PORT 
-        gunicorn -w 1 -b 0.0.0.0:$PORT --timeout 120 --threads=3 --worker-connections=1000  $FLASK_APP:app
+if [ "$MODE" == "both" ]; then
+          #python -m flask run -p $PORT 
+            python redis_worker.py & gunicorn -w 1 -b 0.0.0.0:$PORT --timeout 120 --threads=3 --worker-connections=1000 $FLASK_APP:app
     else
-        if [ "$MODE" == "worker" ];
-            then
-            python redis_worker.py 
-        else
-
-            echo "Specify a mode"
-                
-        fi
+                if  [ "$MODE" == "web" ]; then
+                     #python -m flask run -p $PORT 
+                    gunicorn -w 1 -b 0.0.0.0:$PORT --timeout 120 --threads=3 --worker-connections=1000  $FLASK_APP:app
+                else
+                if [ "$MODE" == "worker" ]; then
+                    python redis_worker.py
+                else
+                     echo "Please specify a mode: [web, worker or both]"
+                fi
     fi
 fi
-
-
